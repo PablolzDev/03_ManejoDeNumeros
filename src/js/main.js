aa7756319b53a72d9851a9bebbcd70200a2cc743
PablolzDev <pabloinprogramacion@gmail.com>
2024-04-17 12:31:29 -0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+//Creacion de numeros
+
+let nuber = 45
+let number= number(45)
+
+console.info(numerber)
+
+// //operaciones
+// const numero1 = 5
+// const numero2 = Number("10")
+
+// console.info(numero1 + numero2)
+// console.info(numero1 - numero2)
+// console.info(numero1 * numero2)
+// console.info(numero1 / numero2)
+
+// let numero1 = Number.parseInt(prompt("Ingresa tu numero"))
+// let numero2 = Number.parseFloat(prompt("Ingresa tu numero decimal"))
+
+// console.info(typeof(numero1))
+// console.info(typeof(numero2))
+
+// console.info(Number.isInteger(numero1))
+
+// console.info(Number.isInteger(numero))
+
+// let numero = 45
+// console.info(numero.toString())
+// console.info(numero.toString(2))
+// console.info(numero.toString(8))
+// console.info(numero.toString(6))
+
+// const numeroEjemplo = 512
+// console.info(numeroEjemplo)
+// console.info(numeroEjemplo.toFixed(2))
+// console.info(numeroEjemplo.toFixed(3))
+// console.info(numeroEjemplo.toFixed(4))
+// console.info(numeroEjemplo.toFixed(5))
+
+
+// const  numeroEjemplo = 412412112123893912398213921123
+
+// console.info(numeroEjemplo)
+// console.info(numeroEjemplo.toExponential())
+// console.info(numeroEjemplo.toExponential(2))
+
+// CLASE Math
+
+// console.log(Math.pow(2,4))
+// console.log(Math.sqrt(2442))
+// console.log(Math.cbrt(68))
+// console.log(Math.cos(1165))
+// console.log(85)
+// console.log(Math.abs(-85))
+
+let numero = -98
+
+if (78>0) {
+    console.log("Es positivo")
+} else {
+    console.log("Es negativo");
+}
+
+console.log(Math.sign(numero))
+
